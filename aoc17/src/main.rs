8b2a95325a3760e5440c7672e2fee6837c462d45
Robert <robert@gmyr.net>
2020-12-20 17:22:58 -0800

    aoc17 part 2

--- aoc17/src/main.rs
+++ aoc17/src/main.rs
@@ -1,63 +1,60 @@
 use std::collections::HashSet;
 use std::fs;
-use std::hash::Hash;
 
-#[derive(PartialEq, Eq, Hash, Copy, Clone)]
-struct Vec3<T> {
-    x: T,
-    y: T,
-    z: T,
+fn main() {
+    println!("part 1: {}", simulate(3, 6).len());
+    println!("part 2: {}", simulate(4, 6).len());
 }
 
-impl Vec3<i64> {
-    fn closed_neighborhood(&self) -> Vec<Vec3<i64>> {
-        let mut neighbors = self.open_neighborhood();
-        neighbors.push(Vec3 {
-            x: self.x,
-            y: self.y,
-            z: self.z,
-        });
-        neighbors
-    }
+trait HasNeighbors: Sized {
+    fn closed_neighborhood(&self) -> Vec<Self>;
+    fn open_neighborhood(&self) -> Vec<Self>;
+}
 
-    fn open_neighborhood(&self) -> Vec<Vec3<i64>> {
+impl HasNeighbors for Vec<i64> {
+    fn closed_neighborhood(&self) -> Vec<Self> {
         let mut neighbors = Vec::new();
-        for x_offset in -1..=1 {
-            for y_offset in -1..=1 {
-                for z_offset in -1..=1 {
-                    if x_offset != 0 || y_offset != 0 || z_offset != 0 {
-                        neighbors.push(Vec3 {
-                            x: self.x + x_offset,
-                            y: self.y + y_offset,
-                            z: self.z + z_offset,
-                        });
-                    }
+        neighbors.push(Vec::new());
+        for element in self.iter() {
+            let mut new_neighbors = Vec::new();
+            for neighbor in &neighbors {
+                for offset in -1..=1 {
+                    let mut clone = neighbor.clone();
+                    clone.push(element + offset);
+                    new_neighbors.push(clone);
                 }
             }
+            neighbors = new_neighbors;
         }
         neighbors
     }
+
+    fn open_neighborhood(&self) -> Vec<Self> {
+        let mut neighbors = self.closed_neighborhood();
+        neighbors.swap_remove(neighbors.len() / 2);
+        neighbors
+    }
 }
 
-fn main() {
-    let mut active_cubes = parse_input("input.txt");
-    for _ in 0..6 {
+fn simulate(rank: u64, steps: u64) -> HashSet<Vec<i64>> {
+    let mut active_cubes = parse_input("input.txt", rank);
+    for _ in 0..steps {
         active_cubes = simulate_step(&active_cubes);
     }
-    println!("part 1: {}", active_cubes.len());
+    active_cubes
 }
 
-fn parse_input(path: &str) -> HashSet<Vec3<i64>> {
+fn parse_input(path: &str, rank: u64) -> HashSet<Vec<i64>> {
     let mut active_cubes = HashSet::new();
     for (y, line) in fs::read_to_string(path).unwrap().lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
             match c {
                 '#' => {
-                    active_cubes.insert(Vec3 {
-                        x: x as i64,
-                        y: y as i64,
-                        z: 0,
-                    });
+                    let mut cube = vec![x as i64, y as i64];
+                    for _ in 2..rank {
+                        cube.push(0);
+                    }
+                    active_cubes.insert(cube);
                 }
                 '.' => (),
                 _ => panic!("invalid input"),
@@ -67,7 +64,7 @@ fn parse_input(path: &str) -> HashSet<Vec3<i64>> {
     active_cubes
 }
 
-fn simulate_step(active_cubes: &HashSet<Vec3<i64>>) -> HashSet<Vec3<i64>> {
+fn simulate_step(active_cubes: &HashSet<Vec<i64>>) -> HashSet<Vec<i64>> {
     let mut candidates = HashSet::new();
     for cube in active_cubes {
         for neighbor in cube.closed_neighborhood() {
@@ -80,7 +77,7 @@ fn simulate_step(active_cubes: &HashSet<Vec3<i64>>) -> HashSet<Vec3<i64>> {
         let num_active_neighbors = cube
             .open_neighborhood()
             .iter()
-            .filter(|c| active_cubes.contains(c))
+            .filter(|c| active_cubes.contains(*c))
             .count();
         if active_cubes.contains(&cube) {
             if num_active_neighbors == 2 || num_active_neighbors == 3 {
